da94e04430db9d2a4fa691a0568edb5812c16947
Ira Noviani <iranoviani11@gmail.com>
2023-12-05 00:46:30 +0700

    add

---
+++ src/components/BuyButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="{ ButtonWoman: woman, ButtonMan: man }">
+        <button>Buy now</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "BuyButton",
+    props: ["woman", "man"],
+};
+</script>
+<style scoped>
+.ButtonWoman button {
+    border: 2px solid #720060;
+    background-color: #720060;
+    font-family: "Prompt", sans-serif;
+    color: white;
+    padding: 10px;
+    margin-right: 20px;
+    margin-top: 20px;
+    width: 90%;
+    border-radius: 5px;
+}
+
+.ButtonMan button {
+    border: 2px solid #002772;
+    background-color: #002772;
+    font-family: "Prompt", sans-serif;
+    color: white;
+    padding: 10px;
+    margin-right: 20px;
+    margin-top: 20px;
+    width: 90%;
+    border-radius: 5px;
+}
+</style>
---
+++ src/components/RatingCategories.vue
@@ -0,0 +1,97 @@
+<template>
+  <div v-if="rating >= 5" class="rating">
+    {{ rating }}/5
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+  </div>
+  <div v-else-if="rating >= 4" class="rating">
+    {{ rating }}/5
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+  </div>
+  <div v-else-if="rating >= 3" class="rating">
+    {{ rating }}/5
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+  </div>
+  <div v-else-if="rating >= 2" class="rating">
+    {{ rating }}/5
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+  </div>
+  <div v-else-if="rating >= 1" class="rating">
+    {{ rating }}/5
+    <div :class="{ ratingWoman: woman, ratingMan: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+  </div>
+  <div v-else class="rating">
+    {{ rating }}/5
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+    <div :class="{ rating2Woman: woman, rating2Man: man }"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "RatingCategories",
+  props: ["woman", "man", "rating"],
+};
+</script>
+<style scoped>
+.ratingWoman {
+  height: 18px;
+  width: 18px;
+  border: 2px solid #720060;
+  background-color: #720060;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 5px;
+}
+
+.rating2Woman {
+  height: 18px;
+  width: 18px;
+  border: 2px solid #720060;
+  background-color: white;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 5px;
+}
+
+.ratingMan {
+  height: 18px;
+  width: 18px;
+  border: 2px solid #002772;
+  background-color: #002772;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 5px;
+}
+
+.rating2Man {
+  height: 18px;
+  width: 18px;
+  border: 2px solid #002772;
+  background-color: white;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 5px;
+}
+</style>
